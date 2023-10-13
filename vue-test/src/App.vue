6eead566cacdb87dd1cf861ec32de88f98ae4738
JuYeong <juyeong3022@osstem.com>
2023-10-13 13:10:55 +0900

    first commit

---
+++ vue-test/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import PHeader from './components/PHeader.vue';
+</script>
+
+<template>
+  <div>
+    <PHeader></PHeader>
+    <RouterView />
+  </div>
+</template>
