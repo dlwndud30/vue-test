f2b625e86c891a41e055d425892289a676e45c0b
JuYeong <juyeong3022@osstem.com>
2023-10-16 10:21:01 +0900

    use bootstrap

--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -1,12 +1,15 @@
 // import './assets/main.css';
 import './scss/styles.scss';
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/dist/js/bootstrap.js';
+import { BootstrapVueNext } from 'bootstrap-vue-next';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
-
 app.use(router);
-
+app.use(BootstrapVueNext);
 app.mount('#app');
